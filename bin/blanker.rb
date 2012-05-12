236e5baa6c53cdf243943c7c7731f55c147bc0e0
SHIOYA, Hiromu <kwappa.856@gmail.com>
2012-05-12 12:32:26 +0900

    first commit
     * add blanker.rb
      * add 'target="_blank"' on <a> tags
     * add start_mongo
      * script to start mongodb manually

---
+++ bin/blanker.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+file_name = ARGV[0]
+
+if !file_name || !File.exists?(file_name)
+  puts "file not found."
+  exit
+end
+
+File.open(file_name) { |f|
+  while line = f.gets
+    puts line.gsub(/href(.+?)>/, %q{href\1 target="_blank">})
+  end
+}
---
+++ installer.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ln -s `pwd`/bin ${HOME}/bin
